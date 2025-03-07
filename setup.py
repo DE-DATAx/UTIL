c93cdb34c067893e3432c57b2c230c60567a9f81
Almir J Gomes <almir.jg@hotmail.com>
2025-03-07 16:04:30 -0300

    Versão contempla a instalação automatica das bibliotecas de dependencia e versão minima do python para ser executada ">=3.9"

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 setup(
     name='DAX_UTIL',
-    version='0.0.0.16',
+    version='0.0.0.25',
     author='Almir J Gomes',
     author_email='almir.jg@hotmail.com',
     packages=find_packages(),
     install_requires=[
         "python-dateutil>=2.9"
     ],
-    python_requeries=">=3.9"
+    python_requeries=">=3.9",
+    description="Biblioteca de funcionalidades",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
 )
